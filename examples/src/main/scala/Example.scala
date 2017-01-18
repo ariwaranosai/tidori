9e573a5b42a049ba5083d161d76c8d43e03aee40
ariwaranosai <nkssai@outlook.com>
2017-01-18 20:53:27 +0800

    add bo implicit

--- examples/src/main/scala/Example.scala
+++ examples/src/main/scala/Example.scala
@@ -2,9 +2,15 @@ import scala.scalajs.js.JSApp
 import org.scalajs.dom._
 import org.scalajs.dom
 import xyz.ariwaranosai.tidori.dom.DomElements._
-import xyz.ariwaranosai.tidori.dom.OperatorContext
-import xyz.ariwaranosai.tidori.dom.DelayDomImplicit._
+import xyz.ariwaranosai.tidori.dom.{BeatDomOperator, OperatorContext}
+import xyz.ariwaranosai.tidori.dom.BeatDomOperator._
+
 import scalatags.JsDom.all._
+import scala.concurrent.ExecutionContext.Implicits.global
+import xyz.ariwaranosai.tidori.dom.DomOperator._
+import xyz.ariwaranosai.tidori.dom.BeatOperatorImplicit._
+
+import scala.concurrent.Future
 
 /**
   * Created by ariwaranosai on 2017/1/8.
@@ -15,30 +21,29 @@ object Example extends JSApp {
   @scala.scalajs.js.annotation.JSExport
   override def main(): Unit = {
     println("hello world!")
-
     val pNode = dom.document.getElementById("broad")
 
-    implicit object T extends OperatorContext {
+    object T extends OperatorContext {
       override val node: Element = pNode
-      override val delta: Double = 178
+      override val delta: Double = 200
     }
 
-    val twitter = a(href:="https://twitter.com/ariwaranosai").render.ds("twitter")
-    val blog = a(href:="http://ariwaranosai.xyz").render.ds("blog")
+    val twitter = a(href:="https://twitter.com/ariwaranosai").render.bs("twitter")
+    val blog = a(href:="http://ariwaranosai.xyz").render.bs("blog")
 
-    val title = "在原佐为-ありわら の さい ".dd ~: htmlBR.d ~:
-      " 不入流的码农,不入流的失业党,不入流的死宅 ".dd ~: htmlBR.d ~:
-      " 脑洞略大,盛产负能量,喜欢读书".dd ~: 4.del() ~: 1.delay ~:
-      "常买书,不怎么读 ".dd ~: htmlBR.d ~:
-      " 机器学习? 数据挖掘? 概率图模型? ".dd ~: htmlBR.d ~:
-      " vim党 血崩党 冷场达人 玩3ds的任黑 ".dd ~: htmlBR.d ~:
-      " 学haskell中途夭折,勉强写写Scala凑数,懒得时候写Python ".dd ~: htmlBR.d ~:
-      " C++什么的逼急了写写,反正都不能帮我毕业 ".dd ~: htmlBR.d ~:
-      " 审美差写不了前端,也不会写后台 ".dd ~: htmlBR.d ~:
-      " OSX Fedora Arch Win HHKB ".dd ~: htmlBR.d ~:
-      " 銀魂最高だ,周三就是该看金光布袋戏 ".dd ~: htmlBR.d ~:
-      " 链接: ".dd ~: twitter ~: " ".dd ~: blog
+    val title = "在原佐为-ありわら の さい ".bb ~: htmlBR.b ~:
+      " 不入流的码农,不入流的失业党,不入流的死宅 ".bb ~: htmlBR.b ~:
+      " 脑洞略大,盛产负能量,喜欢读书".bb ~: 4.del() ~: 1.delay ~:
+      "常买书,不怎么读 ".bb ~: htmlBR.b ~:
+      " 机器学习? 数据挖掘? 概率图模型? ".bb ~: htmlBR.b ~:
+      " vim党 血崩党 冷场达人 玩3ds的任黑 ".bb ~: htmlBR.b ~:
+      " 学haskell中途夭折,勉强写写Scala凑数,懒得时候写Python ".bb ~: htmlBR.b ~:
+      " C++什么的逼急了写写,反正都不能帮我毕业 ".bb ~: htmlBR.b ~:
+      " 审美差写不了前端,也不会写后台 ".bb ~: htmlBR.b ~:
+      " OSX Fedora Arch Win HHKB ".bb ~: htmlBR.b ~:
+      " 銀魂最高だ,周三就是该看金光布袋戏 ".bb ~: htmlBR.b ~:
+      " 链接: ".bb ~: twitter ~: " ".bb ~: blog
 
-    title.run
+    title.run(T)
   }
 }
---
+++ src/main/scala/xyz/ariwaranosai/tidori/dom/BeatOperatorImplicit.scala
@@ -0,0 +1,48 @@
+package xyz.ariwaranosai.tidori.dom
+
+import BeatDomOperator._
+import DomOperator._
+import org.scalajs.dom.Element
+
+/**
+  * Created by ariwaranosai on 2017/1/18.
+  *
+  */
+
+object BeatOperatorImplicit {
+  implicit class StrToBeatOperatorOps(s: String) {
+    def toB(time: Double = 1): BeatDomOperator[Unit] =
+      appendStr(s, time)
+
+    def b: BeatDomOperator[Unit] =
+      appendStr(s)
+
+    def toDB(time: Double = 1): BeatDomOperator[Unit] =
+      sequence(s.map(x => appendStr(x.toString, time)))
+
+    def bb: BeatDomOperator[Unit] =
+      sequence(s.map(x => appendStr(x.toString)))
+  }
+
+  implicit class NodeToBeatOperatorOps(node: Element) {
+    def toB(time: Double = 1): BeatDomOperator[Unit] =
+      appendNode(node, time)
+
+    def b: BeatDomOperator[Unit] =
+      appendNode(node)
+
+    def toBwithS(s: String, time: Double = 1): BeatDomOperator[Unit] =
+      subContext(node, time, s.bb)
+
+    def bs(s: String): BeatDomOperator[Unit] =
+      toBwithS(s)
+  }
+
+  implicit class InActionOps(n: Int) {
+    def del(time: Double = 1): BeatDomOperator[Unit] =
+      repeat(removeLast(time), n)
+
+    def delay: BeatDomOperator[Unit] =
+      DomOperator.delay(n)
+  }
+}
